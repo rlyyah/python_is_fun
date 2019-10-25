6f4401b6c4ee5e2870deec7e6bd552b5b09ccf9d
rlyyah <tmakowski1337@gmail.com>
2019-10-25 11:20:28 +0200

    inicial commit

---
+++ start.py
@@ -0,0 +1,10 @@
+for i in range(10):
+    space = ' '*i
+    print(f"hello{space}world")
+
+bottom = ''
+for el in space:
+    bottom += 'W'
+
+print(f"WWWWW{bottom}WWWWW")
+print(f"WWWWW{bottom}WWWWW")
